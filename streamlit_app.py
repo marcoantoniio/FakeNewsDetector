87950cde237bc4ce07113e962c011668317c412f
Marco Antônio Valério <marcovalerio27j@gmail.com>
2025-10-18 09:42:13 -0300

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,4 @@ import streamlit as st
 st.set_page_config(
     page_title='Fake News Detector',
     page_icon=':earth_americas:',
-
 )
-
-st.title("Fake News Detector")
-
-st.write("Bora começar!!")
-
-
